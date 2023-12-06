04e9995b0bad3169aa501a7af8f74e216f51c2e5
duynhat989 <vietduy989kc@gmail.com>
2023-12-06 21:19:52 +0700

    update

--- src/assets/js/javascipt/group.js
+++ src/assets/js/javascipt/group.js
@@ -60,6 +60,138 @@ const load_pending_post = async (fb_dtsg, user_id, group_id) => {
     return null;
   }
 };
+const decline_post = async (user_id, group_id, story_id, victim_id) => {
+  try {
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("av", user_id);
+    urlencoded.append("__user", user_id);
+    urlencoded.append("__a", "1");
+    urlencoded.append("__req", "1");
+    urlencoded.append("__hs", "19672.HYP:comet_pkg.2.1..2.1");
+    urlencoded.append("dpr", "1.5");
+    urlencoded.append("__ccg", "GOOD");
+    urlencoded.append("__rev", "");
+    urlencoded.append("__s", "");
+    urlencoded.append("__hsi", "");
+    urlencoded.append("__dyn", "");
+    urlencoded.append("__csr", "");
+    urlencoded.append("__comet_req", "15");
+    urlencoded.append("__aaid", "0");
+    urlencoded.append("server_timestamps", "true");
+    urlencoded.append("fb_dtsg", fb_dtsg);
+    urlencoded.append("fb_api_caller_class", "RelayModern");
+    urlencoded.append(
+      "fb_api_req_friendly_name:",
+      "GroupsCometDeclinePendingStoryMutation"
+    );
+    urlencoded.append("doc_id", "6619745291465883");
+    urlencoded.append(
+      "variables",
+      JSON.stringify({
+        currentSection: "PENDING_POSTS",
+        input: {
+          action_source: "GROUP_PENDING_POSTS",
+          group_id: group_id,
+          story_id: story_id,
+          actor_id: user_id,
+          client_mutation_id: "2",
+        },
+        memberID: victim_id,
+        scale: 1,
+      })
+    );
+    const data = await requestPost(
+      urlencoded,
+      "https://www.facebook.com/api/graphql/"
+    );
+    var res = await data.json();
+    return res;
+  } catch (error) {
+    return null;
+  }
+};
+const approve_post = async (user_id, group_id, story_id, victim_id) => {
+  try {
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("av", user_id);
+    urlencoded.append("__user", user_id);
+    urlencoded.append("__a", "1");
+    urlencoded.append("__req", "1");
+    urlencoded.append("__hs", "19672.HYP:comet_pkg.2.1..2.1");
+    urlencoded.append("dpr", "1.5");
+    urlencoded.append("__ccg", "GOOD");
+    urlencoded.append("__rev", "");
+    urlencoded.append("__s", "");
+    urlencoded.append("__hsi", "");
+    urlencoded.append("__dyn", "");
+    urlencoded.append("__csr", "");
+    urlencoded.append("__comet_req", "15");
+    urlencoded.append("__aaid", "0");
+    urlencoded.append("server_timestamps", "true");
+    urlencoded.append("fb_dtsg", fb_dtsg);
+    urlencoded.append("fb_api_caller_class", "RelayModern");
+    urlencoded.append(
+      "fb_api_req_friendly_name:",
+      "GroupsCometApprovePendingStoryMutation"
+    );
+    urlencoded.append("doc_id", "6707778919348457");
+    urlencoded.append(
+      "variables",
+      JSON.stringify({
+        currentSection: "PENDING_POSTS",
+        input: {
+          action_source: "GROUP_PENDING_POSTS",
+          attribution_id_v2:
+            "CometGroupPendingPostsRoot.react,comet.groups.pending_posts,unexpected,1701857585028,269861,,,;CometGroupDiscussionRoot.react,comet.group,unexpected,1701857577856,49829,2361831622,,;GroupsCometCrossGroupFeedRoot.react,comet.groups.feed,tap_bookmark,1701857573313,933004,2361831622,,",
+          give_award: false,
+          group_id: group_id,
+          story_id: story_id,
+          trust_author: false,
+          actor_id: user_id,
+          client_mutation_id: "5",
+        },
+        displayCommentsFeedbackContext: null,
+        displayCommentsContextEnableComment: null,
+        displayCommentsContextIsAdPreview: null,
+        displayCommentsContextIsAggregatedShare: null,
+        displayCommentsContextIsStorySet: null,
+        feedLocation: "GROUP",
+        feedbackSource: 0,
+        focusCommentID: null,
+        hoistStories: [],
+        hoistStoriesCount: 0,
+        hasHoistStories: false,
+        scale: 1,
+        sortingSetting: null,
+        useDefaultActor: false,
+        privacySelectorRenderLocation: "COMET_STREAM",
+        renderLocation: "group",
+        UFI2CommentsProvider_commentsKey:
+          "CometGroupDiscussionRootSuccessQuery",
+        shouldDeferMainFeed: false,
+        memberID: victim_id,
+        groupID: group_id,
+        __relay_internal__pv__GroupsCometLazyLoadFeaturedSectionrelayprovider: false,
+        __relay_internal__pv__IsWorkUserrelayprovider: false,
+        __relay_internal__pv__IsMergQAPollsrelayprovider: false,
+        __relay_internal__pv__CometUFIReactionsEnableShortNamerelayprovider: false,
+        __relay_internal__pv__CometUFIIsRTAEnabledrelayprovider: false,
+        __relay_internal__pv__StoriesArmadilloReplyEnabledrelayprovider: false,
+        __relay_internal__pv__StoriesRingrelayprovider: false,
+        __relay_internal__pv__GroupsCometGroupChatLazyLoadLastMessageSnippetrelayprovider: false,
+      })
+    );
+    const data = await requestPost(
+      urlencoded,
+      "https://www.facebook.com/api/graphql/"
+    );
+    var res = await data.json();
+    return res;
+  } catch (error) {
+    return null;
+  }
+};
+
 const load_pending_member = async (fb_dtsg, user_id, group_id) => {
   try {
     var urlencoded = new URLSearchParams();
@@ -89,8 +221,7 @@ const load_pending_member = async (fb_dtsg, user_id, group_id) => {
       "variables",
       JSON.stringify({
         count: 10,
-        cursor:
-          "",
+        cursor: "",
         filters: [],
         groupID: group_id,
         named: "",
@@ -233,6 +364,8 @@ const requestPost = async (body, url, profile_id = null) => {
 };
 module.exports = {
   load_pending_post,
+  decline_post,
+  approve_post,
   load_pending_member,
   decline_member,
   approve_member,
---
+++ src/assets/js/javascipt/user.js
@@ -0,0 +1,20 @@
+const set_token = async (token) => {
+    try {
+      localStorage.setItem("eaab_token", token);
+    } catch (error) {
+      return null;
+    }
+  };
+  const get_token = async () => {
+      try {
+       return await localStorage.getItem("eaab_token");
+      } catch (error) {
+        return null;
+      }
+    };
+  
+  module.exports = {
+      set_token,
+      get_token
+  };
+  
